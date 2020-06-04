3b871a95cd6aae9bc1b7e4529aff67c85b1bae34
Vojtěch Březina <26902688+VojtechBrezina@users.noreply.github.com>
2020-06-04 09:32:01 +0200

    preparing a new loglevel for ALL the details

--- utils/terminal.py
+++ utils/terminal.py
@@ -11,12 +11,14 @@ LL_output = -1
 LL_error = 0
 LL_warning = 1
 LL_debug = 2
+LL_disect = 3
 
 log_colors = {
     LL_output:Fore.WHITE,
     LL_error:Fore.RED,
     LL_warning:Fore.YELLOW,
-    LL_debug:Fore.GREEN
+    LL_debug:Fore.GREEN,
+    LL_disect:Fore.CYAN,
 }
 
 
@@ -25,7 +27,7 @@ log_level = LL_default
 def log(text: str, level: int, nl: bool = True) -> None:
     """Prints a piece of text in the specified loglevel, folowed by \\n by default."""
     if log_level >= level:
-        print(f"{log_colors[level]}{text}{Fore.RESET}", end=("\n" if nl else ""))
+        print(f"{log_colors[level]}{text}{Fore.RESET}{Back.RESET}", end=("\n" if nl else ""))
 
 def set_log_level(level: int) -> None:
     """Sets the global log level (used by the args parser only)."""
